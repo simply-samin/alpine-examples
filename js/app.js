54fefe1986885ddb8fdef1225efe3e1e36f0f89f
samin_nextgenit <samiulislamsamin@nextgenitltd.com>
2023-08-26 08:43:36 +0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let taskApp = () => {
+    return {
+        tasks: [],
+
+        newTask: "",
+
+        submit() {
+            this.tasks.push({ body: this.newTask, completed: false });
+
+            this.newTask = "";
+        },
+    };
+};
